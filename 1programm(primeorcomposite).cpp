aee1dea4271b913c1c9df7b301ea5ff9dbc5250a
grftwhre <44138690+grftwhre@users.noreply.github.com>
2018-10-14 17:32:43 +0300

    Add files via upload

---
+++ 1programm(primeorcomposite).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <locale.h>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+ int n,r,i;
+ i=1;
+    printf("Enter x\n");
+    scanf("%d", &n);
+if (n==1)
+    printf("prime");
+else
+    r=n%i;
+do{
+i++;
+r=n%i;
+}while (r!=0);
+    if (i==n)
+    printf("%d - prime", n);
+    else
+    printf("%d - composite", n);
+    return 0;
+}
